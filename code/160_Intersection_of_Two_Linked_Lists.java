9d444bc4d708d65d30eb539455d9f61e15d717f2
renwei <36109750+renanxin@users.noreply.github.com>
2020-07-17 11:09:27 +0800

    Create 160_Intersection_of_Two_Linked_Lists.java

---
+++ code/160_Intersection_of_Two_Linked_Lists.java
@@ -0,0 +1,43 @@
+/*
+	本题的目标是求两个链表的公共节点
+	首先获取两个链表的长度，从头开始遍历将较长的那个链表长度变得和较短的那个相等
+	之后将这两个链表同时遍历并做比较，直到二者被遍历到的节点相等或是为null才返回
+	
+*/
+
+class MinStack {
+
+        public long getListNodeLen(ListNode head) {
+        if (head == null) return 0;
+        ListNode pre = head;
+        long i = 1L;
+        while (pre.next != null) {
+            ++i;
+            pre = pre.next;
+        }
+        return i;
+    }
+    
+   // solution for 160
+   public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        long len_A = getListNodeLen(headA);
+        long len_B = getListNodeLen(headB);
+        if(len_A>len_B){
+            while (len_A > len_B) {
+                len_A -= 1;
+                headA = headA.next;
+            }
+        }
+        else if(len_A<len_B){
+            while(len_A<len_B) {
+                len_B -= 1;
+                headB = headB.next;
+            }
+        }
+        while(headA!=null&&headB!=null&&headA!=headB){
+            headA = headA.next;
+            headB = headB.next;
+        }
+        return headA;
+    }
+}
