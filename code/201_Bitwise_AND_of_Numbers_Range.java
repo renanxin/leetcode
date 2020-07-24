0b7606fb4dbb71386732d0eae03eed7c7a48cc12
renwei <36109750+renanxin@users.noreply.github.com>
2020-07-24 10:29:19 +0800

    Update 201_Bitwise_AND_of_Numbers_Range.java

--- code/201_Bitwise_AND_of_Numbers_Range.java
+++ code/201_Bitwise_AND_of_Numbers_Range.java
@@ -1,22 +1,19 @@
 /*
-	此题的目标是给定一组相邻房子中的金额数组，抢劫犯只能抢劫不相邻的房子，求可以抢到的金额最大值
-	对于第i的房间，此处的最大值为前面房子与再前面房子加当前房子金额的最大值，即dp[i] = max(dp[i-1],dp[i-2]+nums[i])
+	又是一道考察位操作Bit Operation的题，相似的题目在LeetCode中还真不少，比如Repeated DNA Sequences 求重复的DNA序列， Single Number 单独的数字, Single Number II 单独的数字之二 ， Grey Code 格雷码，和 Reverse Bits 翻转位 等等，那么这道题其实并不难，我们先从题目中给的例子来分析，[5, 7]里共有三个数字，分别写出它们的二进制为：	
+	101　　110　　111
+	相与后的结果为100，仔细观察我们可以得出，最后的数是该数字范围内所有的数的左边共同的部分，如果上面那个例子不太明显，我们再来看一个范围[26, 30]，它们的二进制如下：
+	11010　　11011　　11100　　11101　　11110
+	发现了规律后，我们只要写代码找到左边公共的部分即可，我们可以从建立一个32位都是1的mask，然后每次向左移一位，比较m和n是否相同，不同再继续左移一位，直至相同，然后把m和mask相与就是最终结果，代码如下：
 	
 */
 
 class MinStack {
 
-   // solution for 198
-    public int rob(int[] nums){
-        if(nums.length==0)  return 0;
-        if(nums.length==1)  return nums[0];
-        if(nums.length==2)  return nums[0]>nums[1]?nums[0]:nums[1];
-        int a = nums[0], b = Math.max(nums[0],nums[1]);
-        for(int i=2;i<nums.length;++i){
-            int tmp = b;
-            b = Math.max(a+nums[i],b);
-            a = b;
-        }
-        return b;
+   // solution for 201
+    public int rangeBitwiseAnd(int m, int n) {
+        int mask = Integer.MAX_VALUE;
+        while((m&mask)!=(n&mask))
+            mask<<=1;
+        return m&mask;
     }
 }
