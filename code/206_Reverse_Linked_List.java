c730070578f0bc4b0a46bb780f9d173c665c5795
renwei <36109750+renanxin@users.noreply.github.com>
2020-07-24 11:22:47 +0800

    Create 206_Reverse_Linked_List.java

---
+++ code/206_Reverse_Linked_List.java
@@ -0,0 +1,24 @@
+/*
+	本题的目标是将字符串反转，可以先这是一个头节点node，然后通过遍历head链表，通过头插法将链表
+	反向插入到node之后，最终返回node.next即可，需要注意head为null或只有一个节点的情况
+	
+*/
+
+class MinStack {
+	
+   // solution for 206
+    public ListNode reverseList(ListNode head) {
+        if(head==null||head.next==null)
+            return head;
+        ListNode node = new ListNode(),root,tmp=head,next;
+        node.next=null;
+        root = node;
+        while(tmp!=null){
+            next = tmp.next;
+            tmp.next = node.next;
+            node.next = tmp;
+            tmp = next;
+        }
+        return node.next;
+    }
+}
