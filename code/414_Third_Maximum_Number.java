1ef36df207258ad2204055f614f1319f590dfb39
renanxin <1796500458@qq.com>
2020-10-14 08:48:22 +0800

    Create 414_Third_Maximum_Number.java

---
+++ code/414_Third_Maximum_Number.java
@@ -0,0 +1,32 @@
+// solution for 381
+class RandomizedCollection {
+
+    // solution for 414
+    int flag = -1;
+    public int thirdMax(int[] nums) {
+        int[] maxNum = {Integer.MIN_VALUE,Integer.MIN_VALUE,Integer.MIN_VALUE};
+        for(int num:nums){
+            joinInNum(maxNum,num);
+        }
+        return flag==2?maxNum[2]:maxNum[0];
+    }
+
+    public void joinInNum(int[] maxNum,int num){
+        int tmp;
+        for(int i=0;i<maxNum.length;++i){
+            if(num>maxNum[i]){
+                tmp = maxNum[i];
+                maxNum[i] = num;
+                num = tmp;
+                if(flag<i){
+                    flag = i;
+                    return;
+                }
+            }if(num==maxNum[i]){
+                if(i==flag+1)
+                    flag = i;
+                return;
+            }
+        }
+    }
+}
