13ce243c3fc556f75ae0200651a168fb92b6f226
renanxin <1796500458@qq.com>
2021-01-24 11:42:10 +0800

    Update 剑指Offer_10-II_青蛙跳台阶问题.java

--- code/剑指Offer_10-II_青蛙跳台阶问题.java
+++ code/剑指Offer_10-II_青蛙跳台阶问题.java
@@ -7,28 +7,20 @@ import java.util.*;
 
 class Solution{
 
-    // 07 重建二叉树
-    public TreeNode buildTree(int[] preorder, int[] inorder) {
-        if(inorder.length==0)
-            return null;
-        Map<Integer, Integer> quickSearch = new HashMap<>();
-        for(int i=0;i<inorder.length;++i)
-            quickSearch.put(inorder[i],i);
-        return reBuildBinaryTree(preorder,quickSearch,0,preorder.length-1,0,preorder.length-1);
-    }
-
-    public TreeNode reBuildBinaryTree(int[] preOrder,Map<Integer,Integer> quickSearch,int begin1,int end1,
-                                      int begin2,int end2){
-        if(begin1>end1)
-            return null;
-        TreeNode root = new TreeNode(preOrder[begin1]);
-        if(begin1==end1){
-            return root;
+    // 10-2 青蛙跳台阶问题
+    public int numWays(int n) {
+        if(n==0)
+            return 1;
+        if(n<=2)
+            return n;
+        int a1 = 1, a2 = 2;
+        for(int i=3;i<=n;++i){
+            int tmp = (a1+a2)%1000000007;
+            if(i%2==1)
+                a1 = tmp;
+            else
+                a2 = tmp;
         }
-        int index = quickSearch.get(preOrder[begin1]);
-        root.left = reBuildBinaryTree(preOrder,quickSearch,begin1+1,begin1+index-begin2,begin2,index-1);
-        root.right = reBuildBinaryTree(preOrder,quickSearch,begin1+index-begin2+1,end1,index+1,end2);
-        return root;
-
+        return n%2==1?a1:a2;
     }
 }
