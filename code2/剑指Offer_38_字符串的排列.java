315b0610a8bb481a9d2d01df3546e6de997e7efa
renanxin <1796500458@qq.com>
2021-01-31 20:44:34 +0800

    Update 剑指Offer_38_字符串的排列.java

--- code2/剑指Offer_38_字符串的排列.java
+++ code2/剑指Offer_38_字符串的排列.java
@@ -9,87 +9,29 @@ import java.util.*;
 
 class Solution{
 
-    // 37 序列化二叉树
-    public class Codec {
+    // 38 字符串的排列
+    public String[] permutation(String s) {
+        HashSet<String> res = new HashSet<>();
+        permutation(s.toCharArray(),res,0);
+        return res.toArray(new String[0]);
+    }
 
-        // Encodes a tree to a single string.
-        public String serialize(TreeNode root) {
-            if (root == null)
-                return "[]";
-            TreeNode nullNode = new TreeNode(Integer.MIN_VALUE);
-            ArrayDeque<TreeNode> nodes = new ArrayDeque<>();
-            ArrayDeque<TreeNode> tmp = new ArrayDeque<>();
-            StringBuilder builder = new StringBuilder();
-            builder.append('[');
-            nodes.addLast(root);
-            int notNullNum = 1;
-            while (notNullNum != 0) {
-                notNullNum = 0;
-                while (!nodes.isEmpty()) {
-                    TreeNode node = nodes.removeFirst();
-                    if (node == nullNode) {
-                        builder.append("null");
-                        builder.append(',');
-                        continue;
-                    }
-                    builder.append(node.val);
-                    builder.append(',');
-                    if (node.left != null) {
-                        tmp.addLast(node.left);
-                        ++notNullNum;
-                    } else
-                        tmp.addLast(nullNode);
-                    if (node.right != null) {
-                        tmp.addLast(node.right);
-                        ++notNullNum;
-                    } else
-                        tmp.addLast(nullNode);
-                }
-                if (notNullNum != 0) {
-                    ArrayDeque<TreeNode> tmpQueue = nodes;
-                    nodes = tmp;
-                    tmp = tmpQueue;
-                }
+    public void permutation(char[] chars,HashSet<String> res,int index){
+        if(index==chars.length-1)
+            res.add(new String(chars));
+        else{
+            int[] tmpChar = new int[26];
+            for(int i=index;i<chars.length;++i){
+                if(tmpChar[chars[i]-'a']==1)
+                    continue;
+                tmpChar[chars[i]-'a']=1;
+                char tmp = chars[i];
+                chars[i] = chars[index];
+                chars[index] = tmp;
+                permutation(chars,res,index+1);
+                chars[index] = chars[i];
+                chars[i] = tmp;
             }
-            builder.setCharAt(builder.length() - 1, ']');
-            return builder.toString();
-        }
-
-        // Decodes your encoded data to tree.
-        public TreeNode deserialize(String data) {
-            String[] nodesStr = data.substring(1, data.length() - 1).split(",");
-            if (nodesStr.length == 0 || data.equals("[]"))
-                return null;
-            ArrayDeque<TreeNode> nodes = new ArrayDeque<>();
-            ArrayDeque<TreeNode> tmp = new ArrayDeque<>();
-            TreeNode res = new TreeNode(Integer.valueOf(nodesStr[0]));
-            int index = 1;
-            nodes.addLast(res);
-            while (!nodes.isEmpty() && index < nodesStr.length) {
-                while (!nodes.isEmpty()) {
-                    TreeNode node = nodes.removeFirst(), left, right;
-                    if (nodesStr[index].equals("null"))
-                        left = null;
-                    else
-                        left = new TreeNode(Integer.valueOf(nodesStr[index]));
-                    index += 1;
-                    if (nodesStr[index].equals("null"))
-                        right = null;
-                    else
-                        right = new TreeNode(Integer.valueOf(nodesStr[index]));
-                    index += 1;
-                    node.left = left;
-                    node.right = right;
-                    if(left!=null)
-                        tmp.addLast(left);
-                    if(right!=null)
-                        tmp.addLast(right);
-                }
-                ArrayDeque<TreeNode> tmpQueue = nodes;
-                nodes = tmp;
-                tmp = tmpQueue;
-            }
-            return res;
         }
     }
 }
