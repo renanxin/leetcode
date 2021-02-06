e4e5487165fdff9398df7ae631ef836401973939
renanxin <1796500458@qq.com>
2021-02-06 15:43:05 +0800

    Create 剑指Offer_41_数据流中的中位数.java

---
+++ code2/剑指Offer_41_数据流中的中位数.java
@@ -0,0 +1,36 @@
+import leetcode.ListNode;
+import leetcode.Node;
+import leetcode.SwordTowardOffer;
+import leetcode.TreeNode;
+
+import java.util.*;
+
+
+
+class Solution{
+
+    // 41 数据流中的中位数
+    class MedianFinder {
+
+        Queue<Integer> A, B;
+
+        public MedianFinder() {
+            A = new PriorityQueue<>(); // 小顶堆，保存较大的一半
+            B = new PriorityQueue<>((x, y) -> (y - x)); // 大顶堆，保存较小的一半
+        }
+
+        public void addNum(int num) {
+            if (A.size() != B.size()) {
+                A.add(num);
+                B.add(A.poll());
+            } else {
+                B.add(num);
+                A.add(B.poll());
+            }
+        }
+
+        public double findMedian() {
+            return A.size() != B.size() ? A.peek() : (A.peek() + B.peek()) / 2.0;
+        }
+    }
+}
